6ee51fb35817b9cb28f1d8a32af3129a7f68c9a5
root <root@DESKTOP-3IBCSAR.localdomain>
2020-02-24 11:58:26 +0200

    added an ER diagram

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 RSpec.feature "Task management function", type: :feature do
+    
   scenario "Test task list" do
     Task.all
   end
+
   scenario "Test task creation" do
     Task.create!(start_date: 'test_task_01', end_date: '',status: 'pending')
     Task.create!(start_date: 'test_task_01', end_date: '',status: 'done')
@@ -10,4 +12,9 @@ RSpec.feature "Task management function", type: :feature do
     expect(page).to have_content 'pending'
     expect(page).to have_content 'done'
   end
+
+  scenario "Test of task details" do
+    task1=Task.first  
+  end
+
 end
