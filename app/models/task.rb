2601b84ad5f43d03f9fd343845c82742668a2bd6
root <root@DESKTOP-3IBCSAR.localdomain>
2020-02-27 12:30:46 +0200

    fixed translation problems

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+    default_scope -> { order(created_at: :desc) }
     validates :start_date, presence: true
     validates :end_date, presence: true  
     validates :status, presence: true  
