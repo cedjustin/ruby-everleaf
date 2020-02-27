bfd482204ccd63ba0c120e5e34bd7df30fbf47de
root <root@DESKTOP-3IBCSAR.localdomain>
2020-02-27 10:44:38 +0200

    created a factory bot

---
+++ spec/factories/task.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+    factory :task do
+        start_date { '1/1/2020' }
+        end_date { '1/2/2020' }
+        status { 'pending' }
+    end
+
+    factory :second_task, class: Task do
+        start_date { '1/1/2020' }
+        end_date { '1/2/2020' }
+        status { 'done' }
+    end
+    
+end
