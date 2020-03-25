fd6b9ccde25dc9c01297446b48e4009333fb6a06
root <root@DESKTOP-3IBCSAR.localdomain>
2020-03-25 10:29:01 +0200

    added email to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+    validates :email, presence:true, uniqueness:true,length:{maximum: 255}
     validates :username, presence:true, uniqueness:true,length:{maximum: 30}
     before_validation {username.downcase!}
     has_secure_password
     validates :password, :password_confirmation, presence:true, length: {minimum: 6}
-    has_manny :tasks, dependent: :destroy
+    has_many :tasks, dependent: :destroy
 end
